da90ab7bae4165450af025224714cb6ea027b085
Luis Atencia@Horbath <atencia17@gmail.com>
2025-09-24 14:18:05 -0500

    feat: agregar metadatos Open Graph para mejorar la visibilidad en redes sociales

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -8,6 +8,26 @@ export function meta({}: Route.MetaArgs) {
       name: "description",
       content: "Welcome to the Runescape 3 Leagues Player lookup!",
     },
+    {
+      name: "og:title",
+      content: "Runescape 3 Leagues Player lookup",
+    },
+    {
+      name: "og:description",
+      content: "Welcome to the Runescape 3 Leagues Player lookup!",
+    },
+    {
+      name: "og:image",
+      content: "https://rsll.luedan.dev/logo.png",
+    },
+    {
+      name: "og:type",
+      content: "website",
+    },
+    {
+      name: "og:url",
+      content: "https://rsll.luedan.dev/",
+    }
   ];
 }
 
