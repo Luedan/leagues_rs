4b626a4c005aff86cf726cda398aa801445c2a04
Luis Atencia <atencia17@gmail.com>
2025-09-23 22:54:38 -0500

    feat: actualizar metadatos de la página de inicio para reflejar el enfoque en Runescape 3 Leagues

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Runescape 3 Leagues Player lookup" },
+    { name: "description", content: "Welcome to the Runescape 3 Leagues Player lookup!" },
   ];
 }
 
